aaf5a706d202befe9c03080942b17433b07a7e2d
frankie99g <97924484+frankie99g@users.noreply.github.com>
2024-02-20 21:11:00 -0700

    Adding navigation links

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -4,3 +4,6 @@ st.markdown("# Welcome to my Website!")
 st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer stats")
+
+link = '[To my Github Pages Site](https://frankie99g.github.io/github_assignment/)'
+st.markdown(link, unsafe_allow_html=True)
